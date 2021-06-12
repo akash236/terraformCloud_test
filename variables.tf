8f57dd8537e80077a54634def345821f42222a7e
akash236 <akash.56789@gmail.com>
2021-06-12 16:40:53 +0530

    adding variables

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
+  type        = string
+}
+
+variable "ARM_CLIENT_ID" {
+  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
+  type        = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
+  type        = string
+}
+
+variable "ARM_TENANT_ID" {
+  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
+  type        = string
+}
