dbfc591cde0b940dcef0856570397f86b6be25d6
akash236 <akash.56789@gmail.com>
2021-06-12 17:35:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,1 @@
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
-  type        = string
-}
 
-variable "ARM_CLIENT_ID" {
-  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
-  type        = string
-}
-
-variable "ARM_CLIENT_SECRET" {
-  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
-  type        = string
-}
-
-variable "ARM_TENANT_ID" {
-  description = "(Required) The subscription id where the SQL Managed Instance is deployed."
-  type        = string
-}
